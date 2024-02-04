4b0c35f4485e56fb6afb1aef2a8e139602b8319a
Krasnorutskiy Kirill <dullsharpz@gmail.com>
2024-02-04 14:13:10 +0300

    fix new idp button

--- src/pages/head-empoyees-employee/head-empoyees-employee.tsx
+++ src/pages/head-empoyees-employee/head-empoyees-employee.tsx
@@ -1,68 +1,68 @@
-import { FC, useEffect, useState } from "react";
-import styles from "./head-empoyees-employee.module.scss";
-import Tabs from "../../ui/tabs/tabs";
-import TabPane from "../../ui/tabs/tab-pane/tab-pane";
-import TextArea from "../../ui/text-area/text-area";
-import CompetenciesTable from "../../components/competencies-table/competencies-table";
-import IdpCard from "../../components/idp-card/idp-card";
-import ButtonBack from "../../ui/buttons/button-back/button-back";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
-import ButtonAccent from "../../ui/buttons/button-accent/button-accent";
-import { routes } from "../../utils/const-routes";
-import { useAppSelector, useAppDispatch } from "../../redux/hooks";
-import { getUserById } from "../../api/api";
+import { FC, useEffect, useState } from 'react'
+import styles from './head-empoyees-employee.module.scss'
+import Tabs from '../../ui/tabs/tabs'
+import TabPane from '../../ui/tabs/tab-pane/tab-pane'
+import TextArea from '../../ui/text-area/text-area'
+import CompetenciesTable from '../../components/competencies-table/competencies-table'
+import IdpCard from '../../components/idp-card/idp-card'
+import ButtonBack from '../../ui/buttons/button-back/button-back'
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import ButtonAccent from '../../ui/buttons/button-accent/button-accent'
+import { routes } from '../../utils/const-routes'
+import { useAppSelector, useAppDispatch } from '../../redux/hooks'
+import { getUserById } from '../../api/api'
 import {
   setActiveEmployee,
   setErrorMessageActiveEmployee,
   setIsFailedActiveEmployee,
   setIsRequestActiveEmployee,
   setIsSuccessActiveEmployee,
-} from "../../redux/slices/head-employees-employee-slice";
-import ErrorPage from "../not-found/error";
-import LoaderCircle from "../../components/loader/loader";
+} from '../../redux/slices/head-employees-employee-slice'
+import ErrorPage from '../not-found/error'
+import LoaderCircle from '../../components/loader/loader'
 
 const HeadEmpoyeesEmployee: FC = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const params = useParams();
-  const dispatch = useAppDispatch();
-  const { user, isRequest, isFailed, isSuccess, errorMessage } = useAppSelector(
-    (state) => state.activeEmployee
-  );
+  const location = useLocation()
+  const navigate = useNavigate()
+  const params = useParams()
+  const dispatch = useAppDispatch()
+  const { user, isRequest, isFailed, isSuccess, errorMessage } =
+    useAppSelector((state) => state.activeEmployee)
   const [isAddIdpButtonDisabled, setIsAddIdpButtonDisabled] =
-    useState<boolean>(false);
+    useState<boolean>(false)
 
   const handleNewIdpClick = () => {
-    navigate(location.pathname + "/form");
-  };
+    navigate(location.pathname + '/form')
+  }
 
   async function receivingActiveEmployee() {
-    dispatch(setIsRequestActiveEmployee(true));
+    dispatch(setIsRequestActiveEmployee(true))
     try {
-      const employee = await getUserById(+params.user_id);
-      dispatch(setActiveEmployee({ user: employee.data }));
-      dispatch(setIsSuccessActiveEmployee(true));
+      const employee = await getUserById(+params.user_id)
+      dispatch(setActiveEmployee({ user: employee.data }))
+      dispatch(setIsSuccessActiveEmployee(true))
     } catch (error) {
-      dispatch(setIsFailedActiveEmployee(true));
+      dispatch(setIsFailedActiveEmployee(true))
       dispatch(
         setErrorMessageActiveEmployee(`Ошибка ${error.toJSON().status}`)
-      );
+      )
     } finally {
-      dispatch(setIsRequestActiveEmployee(false));
+      dispatch(setIsRequestActiveEmployee(false))
     }
   }
 
   useEffect(() => {
     if (user.id !== +params.user_id) {
-      receivingActiveEmployee();
+      receivingActiveEmployee()
     }
-  }, [user, params]);
+  }, [user, params])
 
   useEffect(() => {
+    setIsAddIdpButtonDisabled(false)
     user.idps.forEach((item) =>
-      item.status_idp === "in_work" ? setIsAddIdpButtonDisabled(true) : setIsAddIdpButtonDisabled(false)
-    );
-  }, [user]);
+      item.status_idp === 'in_work' ? setIsAddIdpButtonDisabled(true) : ''
+    )
+  }, [user])
 
   return (
     <>
@@ -78,34 +78,34 @@ const HeadEmpoyeesEmployee: FC = () => {
               alt={`${user.last_name} ${user.first_name} ${user?.patronymic}`}
             />
             <h1
-              className={styles.title}
-            >{`${user.last_name} ${user.first_name} ${user?.patronymic}`}</h1>
+              className={
+                styles.title
+              }>{`${user.last_name} ${user.first_name} ${user?.patronymic}`}</h1>
           </div>
           <Tabs extraContainerStyles={styles.extra_container_styles}>
-            <TabPane title="Личная информация">
+            <TabPane title='Личная информация'>
               <section
-                aria-label="Личная информация"
-                className={styles.tab_child}
-              >
-                <TextArea context={"Департамент"} content={user.department} />
+                aria-label='Личная информация'
+                className={styles.tab_child}>
+                <TextArea context={'Департамент'} content={user.department} />
                 <div className={styles.extra_text_wrapper}>
-                  <TextArea context={"Должность"} content={user.post} />
-                  <TextArea context={"Грейд"} content={user.grade} />
+                  <TextArea context={'Должность'} content={user.post} />
+                  <TextArea context={'Грейд'} content={user.grade} />
                 </div>
               </section>
               <section className={styles.competencies_container}>
                 <h2 className={styles.competencies_title}>Компетенции</h2>
                 <CompetenciesTable
-                  title={"Soft Skills"}
+                  title={'Soft Skills'}
                   scors={user.soft_skills}
                 />
                 <CompetenciesTable
-                  title={"Hard Skills"}
+                  title={'Hard Skills'}
                   scors={user.hard_skills}
                 />
               </section>
             </TabPane>
-            <TabPane title="Индивидуальный план развития">
+            <TabPane title='Индивидуальный план развития'>
               <ul className={styles.idp_list}>
                 {user.idps.map((item) => (
                   <IdpCard data={item} key={item.title} isHead={true} />
@@ -113,9 +113,9 @@ const HeadEmpoyeesEmployee: FC = () => {
               </ul>
               <div className={styles.button_accent}>
                 <ButtonAccent
-                  title="Назначить новый ИПР"
+                  title='Назначить новый ИПР'
                   disabled={isAddIdpButtonDisabled}
-                  path={location.pathname + "/form"}
+                  path={location.pathname + '/form'}
                 />
               </div>
             </TabPane>
@@ -123,7 +123,7 @@ const HeadEmpoyeesEmployee: FC = () => {
         </>
       )}
     </>
-  );
-};
+  )
+}
 
-export default HeadEmpoyeesEmployee;
+export default HeadEmpoyeesEmployee
